5f50edf4a245e138556ab36ffddd500ef0f50b1e
hoanghd <hoanghd164@gmail.com>
2023-01-04 11:52:00 +0000

    The first commit

--- docker-build.py
+++ docker-build.py
@@ -9,19 +9,36 @@ ci_registry_password = 'Ho@nghd90'
 
 #Docker image information
 image_name = 'build-infra'
+image_name = 'asterisk'
 image_version = 'latest'
 
 def docker_build():
-    Dockerfile = '''FROM ubuntu:20.04
-    RUN apt-get update -y && apt-get install -y python3.8 python-setuptools snmp screen vim psmisc tree git tmux cron net-tools
-    RUN apt install -y python3-pip iputils-ping traceroute
+    # Dockerfile = '''FROM ubuntu:20.04
+    # RUN apt-get update -y && apt-get install -y python3.8 python-setuptools snmp screen vim psmisc tree git tmux cron net-tools
+    # RUN apt install -y python3-pip iputils-ping traceroute
 
-    RUN pip3 install --upgrade pip paramiko jsonpath-ng requests
+    # RUN pip3 install --upgrade pip
+    # RUN pip3 install paramiko jsonpath-ng requests
 
-    WORKDIR /home
+    # WORKDIR /home
+
+    # RUN echo 'set number' > ~/.vimrc && echo 'syntax on' >> ~/.vimrc && echo 'highlight Comment ctermfg=LightCyan' >> ~/.vimrc && echo 'set relativenumber' >> ~/.vimrc && echo 'set shiftwidth=2' >> ~/.vimrc
+    # ENTRYPOINT echo 'Hà Đăng Hoàng' && /bin/bash'''
+
+    Dockerfile = '''FROM hoanghd164/asterisk:16pbx
+    RUN yum install -y python3.8 python-setuptools snmp screen vim psmisc tree git tmux cron net-tools python3-pip iputils-ping traceroute initscripts
+    RUN pip3 install --upgrade pip
+    RUN pip3 install paramiko jsonpath-ng requests
+
+    WORKDIR /etc/asterisk/
 
     RUN echo 'set number' > ~/.vimrc && echo 'syntax on' >> ~/.vimrc && echo 'highlight Comment ctermfg=LightCyan' >> ~/.vimrc && echo 'set relativenumber' >> ~/.vimrc && echo 'set shiftwidth=2' >> ~/.vimrc
-    ENTRYPOINT echo 'Hà Đăng Hoàng' && /bin/bash'''
+
+    RUN echo '#!/bin/bash' > /usr/local/bin/run_asterisk \
+    && echo "/etc/init.d/asterisk start" >> /usr/local/bin/run_asterisk \
+    && chmod +x /usr/local/bin/run_asterisk
+
+    ENTRYPOINT run_asterisk && /bin/bash'''
 
     file_option.file_write('Dockerfile',Dockerfile)
     os.system('docker build -t %s:%s --force-rm -f Dockerfile .' %(image_name,image_version))
@@ -33,4 +50,4 @@ def docker_push_image():
     os.system('docker push %s/%s:%s' %(ci_registry,image_name,image_version))
 
 docker_build()
-docker_push_image()
+# docker_push_image()
---
+++ docker.sh
@@ -0,0 +1,98 @@
+docker run -it --name=asterisk --hostname=asterisk --network=host centos:centos7
+docker run -itd --name=pbx-asterisk --network=host -p 5060:5060/tcp -p 5060:5060/udp -p 10000:30000/udp hoanghd164/asterisk:16pbx bash
+docker run -itd --name=pbx-asterisk -p 5060:5060/tcp -p 5060:5060/udp -p 10000:30000/udp asterisk:latest bash
+
+yum  -y install epel-release
+yum -y install wget vim net-tools
+yum -y groupinstall "Development Tools"
+yum -y install libedit-devel sqlite-devel psmisc gmime-devel ncurses-devel libtermcap-devel sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel uuid-devel libtool libuuid-devel subversion kernel-devel kernel-devel-$(uname -r) git subversion kernel-devel crontabs cronie cronie-anacron wget vim
+
+cd /usr/src/
+git clone https://github.com/akheron/jansson.git
+cd jansson
+autoreconf  -i
+./configure --prefix=/usr/
+make && make install
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-15-current.tar.gz
+cd /usr/src/ 
+git clone https://github.com/pjsip/pjproject.git
+cd pjproject
+./configure CFLAGS="-DNDEBUG -DPJ_HAS_IPV6=1" --prefix=/usr --libdir=/usr/lib64 --enable-shared --disable-video --disable-sound --disable-opencore-amr
+make dep
+make
+make install
+ldconfig
+
+cd /usr/src/
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-16-current.tar.gz
+tar xvfz asterisk-16-current.tar.gz
+rm -f asterisk-16-current.tar.gz
+cd asterisk-*
+./configure --libdir=/usr/lib64
+
+make menuselect
+
+contrib/scripts/get_mp3_source.sh
+make
+make install
+make samples
+make config
+ldconfig
+
+groupadd asterisk
+useradd -r -d /var/lib/asterisk -g asterisk asterisk
+usermod -aG audio,dialout asterisk
+chown -R asterisk.asterisk /etc/asterisk
+chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
+chown -R asterisk.asterisk /usr/lib64/asterisk
+
+vim /etc/sysconfig/asterisk
+AST_USER="asterisk"
+AST_GROUP="asterisk"
+
+vim /etc/asterisk/asterisk.conf
+runuser = asterisk ; The user to run as.
+rungroup = asterisk ; The group to run as.
+
+/etc/init.d/asterisk start
+
+systemctl restart asterisk
+systemctl enable asterisk
+asterisk -rvv
+asterisk -vvvvvr
+
+yum install -y initscripts
+
+docker container commit -m "Comment" faa2b9ec6018 hoanghd164/asterisk:16pbx
+
+docker login -u hoanghd164 -p 'Ho@nghd90'
+docker image ls
+docker tag centos:centos7 hoanghd164/asterisk:16pbx
+docker image ls
+docker push hoanghd164/asterisk:16pbx
+
+[100]
+username=100
+secret=1234
+type=friend
+host=dynamic
+context=test
+qualify=yes
+directmedia=no
+disallow=all
+allow=ulaw,alaw
+
+[101]
+username=101
+secret=1234
+type=friend
+host=dynamic
+context=test
+qualify=yes
+directmedia=no
+disallow=all
+allow=ulaw,alaw
+
+
+[test]
+exten => _1XX,1,Dial(SIP/${EXTEN})
